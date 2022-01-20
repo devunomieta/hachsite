6871911264221c6cd632b9cc5cab70eb1d0b5288
Unomieta Joseph <lionelunomieta@gmail.com>
2022-01-20 20:36:39 +0100

    event page updated

--- assets/js/read_more.js
+++ assets/js/read_more.js
@@ -29,3 +29,19 @@ function myFunction2() {
         moreText.style.display = "inline";
     }
 }
+
+function myFunction3() {
+    var dots = document.getElementById("dots3");
+    var moreText = document.getElementById("more3");
+    var btnText = document.getElementById("myBtn3");
+
+    if (dots.style.display === "none") {
+        dots.style.display = "inline";
+        btnText.innerHTML = "Read more"; 
+        moreText.style.display = "none";
+    } else {
+        dots.style.display = "none";
+        btnText.innerHTML = "Read less"; 
+        moreText.style.display = "inline";
+    }
+}
